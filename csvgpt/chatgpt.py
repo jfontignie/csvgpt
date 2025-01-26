7791d23a5a0c46cb159aab491b414a0d4d64f2bb
jacques <jacques.fontignie@gmail.com>
2025-01-26 19:44:11 +0100

    refactoring

--- csvgpt/chatgpt.py
+++ csvgpt/chatgpt.py
@@ -54,7 +54,7 @@ class ChatGPT:
 
         chat_completion = self.client.chat.completions.create(
             messages=self.messages,
-            model="gpt-3.5-turbo",
+            model="gpt-4o-mini",
             # model="gpt-4",
         )
         return chat_completion.choices[0].message.content
